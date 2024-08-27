a58d4320ea9a8342ccead3942fad0f5e6d2431ec
Vladimir nett00n Budylnikov <git@nett00n.org>
2024-08-27 15:01:51 +0400

    2024-08-27 15:01:51+04:00

---
+++ configuration.apotheon.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  system.stateVersion = "24.05";
+
+  networking = {
+    hostName = "Apotheon";
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "Asia/Tbilisi";
+  imports = [
+    ./boot.nix
+    ./fonts.nix
+    ./hardware-configuration.nix
+    ./locale.nix
+    ./mounts.nix
+    ./nix-ld.nix
+    ./ollama.nix
+    ./programs.nix
+    ./sound.nix
+    ./ssh.nix
+    ./users.nix
+    ./virtualisation.nix
+    ./X11.nix
+  ];
+}
