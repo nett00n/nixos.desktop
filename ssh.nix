017b59423477fe0ce6bfebfc271c9c11232233c4
Vladimir nett00n Budylnikov <git@nett00n.org>
2024-08-27 19:19:58 +0400

    2024-08-27 19:19:58+04:00

--- ssh.nix
+++ ssh.nix
@@ -1,6 +1,5 @@
 {
   services.openssh.enable = true;
-  services.openssh.settings.AllowGroups = [ "ssh-users" ];
   services.openssh.settings.AllowUsers = [ "nett00n" ];
   services.openssh.settings.PasswordAuthentication = false;
   services.openssh.settings.PermitRootLogin = "no";
