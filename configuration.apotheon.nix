166f11c6b3bbac981a0b8430acf4ff8fb33ac65e
Vladimir nett00n Budylnikov <git@nett00n.org>
2024-09-10 11:29:39 +0400

    2024-09-10 11:29:39+04:00

--- configuration.apotheon.nix
+++ configuration.apotheon.nix
@@ -24,4 +24,11 @@
     ./virtualisation.nix
     ./X11.nix
   ];
+  # Make macbook keyboard usable: ctrl, fn, super, alt and fn buttons work like fn, not media
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=2
+    options hid_apple swap_opt_cmd=1
+    options hid_apple swap_fn_leftctrl=1
+  '';
+
 }
