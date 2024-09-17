2ff139b815cd9ea964c0aa921d5b89534b3b6bd6
Vladimir nett00n Budylnikov <git@nett00n.org>
2024-09-17 12:13:53 +0400

    2024-09-17 12:13:53+04:00

--- common.nix
+++ common.nix
@@ -3,4 +3,5 @@
   networking.networkmanager.enable = true;
   time.timeZone = "Asia/Tbilisi";
   hardware.enableAllFirmware = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
