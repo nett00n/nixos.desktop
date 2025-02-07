742b485147b606f28781f8afcf229276be6b05cc
Vladimir nett00n Budylnikov <git@nett00n.org>
2025-02-07 15:23:44 +0400

    2025-02-07 15:23:44+04:00

--- ollama.nix
+++ ollama.nix
@@ -4,5 +4,12 @@
   services.ollama = {
     enable = true;
     acceleration = "cuda";
+    loadModels = [ "llama3.1" "llama3.3" "mistral" "deepseek-r1:14b" ];
+  };
+  services.open-webui = {
+    enable = true;
+    host = "0.0.0.0";
+    port = 8081;
+    openFirewall = true;
   };
 }
